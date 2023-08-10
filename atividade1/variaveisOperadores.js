f10ae4fe792e61a57297b9e195d9e60d0b5d7c3d
FELIPE HENRIQUE CAMPOS CÉSAR <felipe.cesar@a.ucb.br>
2023-08-10 10:47:57 -0300

    Atividade Feita

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+const n1 = 10;
+const n2 = 5;
+
+const adicao = n1 + n2;
+const subtracao = n1 - n2;
+const multiplicacao = n1 * n2;
+const divisao = n1 / n2;
+
+const adicaoString = "Adição";
+const subtracaoString = "Subtração";
+const multiplicacaoString = "Multiplicação";
+const divisaoString = "Divisão";
+
+console.log(`O resultado da ${adicaoString} entre ${n1} e ${n2} é igual a: ${adicao}`);
+console.log(`O resultado da ${subtracaoString} entre ${n1} e ${n2} é igual a: ${subtracao}`);
+console.log(`O resultado da ${multiplicacaoString} entre ${n1} e ${n2} é igual a: ${multiplicacao}`);
+console.log(`O resultado da ${divisaoString} entre ${n1} e ${n2} é igual a: ${divisao}`);
+
+
+
