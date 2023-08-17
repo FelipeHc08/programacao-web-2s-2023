dc969af4aae11992fbe27ebb109bcd185c0faec5
FELIPE HENRIQUE CAMPOS CÉSAR <felipe.cesar@a.ucb.br>
2023-08-17 09:42:04 -0300

    Exercício atualizado

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -6,15 +6,10 @@ const subtracao = n1 - n2;
 const multiplicacao = n1 * n2;
 const divisao = n1 / n2;
 
-const adicaoString = "Adição";
-const subtracaoString = "Subtração";
-const multiplicacaoString = "Multiplicação";
-const divisaoString = "Divisão";
-
-console.log(`O resultado da ${adicaoString} entre ${n1} e ${n2} é igual a: ${adicao}`);
-console.log(`O resultado da ${subtracaoString} entre ${n1} e ${n2} é igual a: ${subtracao}`);
-console.log(`O resultado da ${multiplicacaoString} entre ${n1} e ${n2} é igual a: ${multiplicacao}`);
-console.log(`O resultado da ${divisaoString} entre ${n1} e ${n2} é igual a: ${divisao}`);
+console.log(`O resultado da adição entre ${n1} e ${n2} é igual a: ${adicao}`);
+console.log(`O resultado da subtração entre ${n1} e ${n2} é igual a: ${subtracao}`);
+console.log(`O resultado da multiplicação entre ${n1} e ${n2} é igual a: ${multiplicacao}`);
+console.log(`O resultado da divisão entre ${n1} e ${n2} é igual a: ${divisao}`);
 
 
 
