bb336825c62a357d9a97f35ce2e64db645506e7b
FELIPE HENRIQUE CAMPOS CÉSAR <felipe.cesar@a.ucb.br>
2023-08-24 09:47:48 -0300

    Exercício 1 corrigido

--- atividades/atividade2/exercicio1.js
+++ atividades/atividade2/exercicio1.js
@@ -4,32 +4,41 @@ Exercício 2
 Faça um algoritmo que imprime uma matriz A e sua transposta.
 */
 
-
-function matrizTransposta(matriz) {//função para transpor a matriz com o parâmetro matriz   
-    const linhas = matriz.length;//obtém o número de linhas da matriz
-    const colunas = matriz[0].length;//obtém o número de colunas da matriz
+// Função para calcular a matriz transposta
+function matrizTransposta(matriz) {
+    const linhas = matriz.length; // Obtém o número de linhas da matriz original
+    const colunas = matriz[0].length; // Obtém o número de colunas da matriz original
     
-    for (let i = 0; i < linhas; i++) {//acessa as linhas da matriz
-        for (let j = i + 1; j < colunas; j++) { //acessa as colunas da matriz (j = i + 1 para acessar os valores acima da diagonal principal)
-            //troca os elementos para realizar a transposição, seguindo a seguinte lógica:
-            const temp = matriz[i][j];//variável temporária para armazenar o valor da matriz
-            matriz[i][j] = matriz[j][i];//atribui o valor [i][j] a [j][i]
-            matriz[j][i] = temp;//atribui o valor [j][i] para a variável temporária
+    // Cria uma nova matriz vazia com as dimensões invertidas
+    const matrizTransposta = new Array(colunas); // Cria um array para as colunas da matriz transposta
+    for (let i = 0; i < colunas; i++) {
+        matrizTransposta[i] = new Array(linhas); // Para cada coluna, cria um array para as linhas
+    }
+    
+    // Preenche a matriz transposta com os elementos da matriz original nas posições corretas
+    for (let i = 0; i < linhas; i++) {
+        for (let j = 0; j < colunas; j++) {
+            matrizTransposta[j][i] = matriz[i][j]; // Troca os índices para transpor os elementos
         }
     }
+    
+    return matrizTransposta; // Retorna a matriz transposta resultante
 }
 
-const matriz = [//declara a matriz que será transposta
+const matriz = [
     [1, 2],
     [3, 4],
     [5, 6]
 ];
 
-matrizTransposta(matriz);//"chama" a função
-
-console.log("Matriz transposta: ");
-
-for (let i = 0; i < matriz.length; i++) { //acessa e exibe a matriz já transportada
-    console.log(matriz[i]);
+console.log('Matriz original:');
+for (let i = 0; i < matriz.length; i++) {
+    console.log(matriz[i]); // Exibe cada linha da matriz original
 }
 
+const matrizTranspostaResultante = matrizTransposta(matriz); // Calcula a matriz transposta
+
+console.log("Matriz transposta:");
+for (let i = 0; i < matrizTranspostaResultante.length; i++) {
+    console.log(matrizTranspostaResultante[i]); // Exibe cada linha da matriz transposta
+}
